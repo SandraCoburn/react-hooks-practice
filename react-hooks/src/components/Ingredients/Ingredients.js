dd5b52758577881395c4124c278cafc56898054f
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-14 19:27:21 -0700

    Added a removeItem function to remove ingreedients

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
 import Search from "./Search";
 
 const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const removeItemHandler = (id) => {
+    let newIngredients = ingredients.filter((ing) => ing.id !== id);
+    setIngredients(newIngredients);
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
       <section>
         <Search />
       </section>
+      <IngredientList
+        ingredients={ingredients}
+        onRemoveItem={removeItemHandler}
+      />
     </div>
   );
 };
